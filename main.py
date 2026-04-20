73e9ccc428cab6b1a6add6db33db6662ae7704eb
aozodbek2197 <aozodbek2197@gmail.com>
2026-04-20 23:49:36 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+# 1-mashq
+def min_window(s, t):
+    from collections import Counter
+    if not t or not s: return ""
+    dict_t = Counter(t)
+    required = len(dict_t)
+    l, r = 0, 0
+    formed = 0
+    window_counts = {}
+    ans = float("inf"), None, None
+    while r < len(s):
+        character = s[r]
+        window_counts[character] = window_counts.get(character, 0) + 1
+        if character in dict_t and window_counts[character] == dict_t[character]:
+            formed += 1
+        while l <= r and formed == required:
+            character = s[l]
+            if r - l + 1 < ans[0]:
+                ans = r - l + 1, l, r
+            window_counts[character] -= 1
+            if character in dict_t and window_counts[character] < dict_t[character]:
+                formed -= 1
+            l += 1
+        r += 1
+    return "" if ans[0] == float("inf") else s[ans[1]:ans[2]+1]
+
+print(min_window("ADOBECODEBANC", "ABC"))
+# 2-mashq
+def longest_valid_parentheses(s):
+    stack = [-1]
+    max_len = 0
+    for i in range(len(s)):
+        if s[i] == '(':
+            stack.append(i)
+        else:
+            stack.pop()
+            if not stack:
+                stack.append(i)
+            else:
+                max_len = max(max_len, i - stack[-1])
+    return max_len
+
+print(longest_valid_parentheses("(()"))
